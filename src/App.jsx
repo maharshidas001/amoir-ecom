6572876bdebde9fb8bf7d35dc69a3ee2fedebf3f
Maharshi Das <maharshidas001@gmail.com>
2024-05-18 09:23:16 +0530

    Setup React router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <>
-      App
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
